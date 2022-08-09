578901e008e5a90187ad2e45f8d76dbcd4990be6
AviyaArusi <110820987+AviyaArusi@users.noreply.github.com>
2022-08-10 00:17:42 +0300

    Update Folder.java

--- Folder.java
+++ Folder.java
@@ -38,18 +38,18 @@ public class Folder {
     }
 
 
-    //One '*' represents a file. Two '**' represents a folder.
+    //The symbol '◇' represents a file. The symbol '◆' represents a folder.
     //The int spaces represent the distance in print  of files and folder that in a specific folder.
     public void folderToString(int spaces) {
         //Print the root
-        System.out.print("**"+folderName+":");
+        System.out.print("◆ "+folderName+":");
        //Print all files from root.
         for (HashMap.Entry<String, String> set : files.entrySet()) {
             System.out.println("");
             for (int i = 0; i < spaces; i++) {
                 System.out.print(" ");
             }
-                System.out.print("*"+set.getKey());
+                System.out.print("◇ "+set.getKey());
         }
         System.out.println("");
         //print all folders from root.
